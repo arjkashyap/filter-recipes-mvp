3e9a031f33d287c87b356334191d9ed6d73b3ca5
arjun <arjun.a@reltio.com>
2024-09-13 01:40:07 +0530

    commit-1 setting up boiler plate

---
+++ src/main/java/com/filter/recipe/mvp/filter/ecipe/mvp/FilterRecipeMvpApplication.java
@@ -0,0 +1,13 @@
+package com.filter.recipe.mvp.filter.ecipe.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterRecipeMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterRecipeMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filter/recipe/mvp/filter/ecipe/mvp/FilterRecipeMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filter.recipe.mvp.filter.ecipe.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterRecipeMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
